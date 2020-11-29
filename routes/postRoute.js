ff2db48e5972daac30bef06978b56be324fde5e7
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-11-29 10:02:08 -0800

    feat(post-route): create post routes
    created post routes and post modals, it returns post with its comments now

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,5 +1,38 @@
 const express = require("express");
+const Post = require("../models/postModal");
 
 const route = express.Router();
+
+
+route.get('/get_post', async (req, res) => {
+   try {
+      const posts = await Post.getAll();
+      res.status(200).json(posts);
+   } catch (error) {
+      res.status(500).json({errMessage: error.message});
+   }
+});
+
+route.post('/create/:user_id', async (req, res) => {
+   const {user_id} = req.params;
+
+   try {
+      const postId = await Post.add(user_id, req.body);
+      res.status(201).json(postId);
+   } catch (error) {
+      res.status(500).json({errMessage: error.message});
+   }
+})
+
+route.post("/add_comment/:post_id", async (req, res) => {
+   const {post_id} = req.params;
+
+   try {
+      const commentId = await Post.addComment(post_id, req.body);
+      res.status(201).json(commentId);
+   } catch (error) {
+      res.status(500).json({errMessage: error.message});
+   }
+})
 
 module.exports = route;
